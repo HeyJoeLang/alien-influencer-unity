e673a8f06cba2b09896675664351bb405186c6b4
HeyJoeLang <heyjoelang@gmail.com>
2026-03-12 14:20:10 -0400

    Set Fire Distance Parameter

--- Alien Influencer/Assets/Base/Scripts/Fire.cs
+++ Alien Influencer/Assets/Base/Scripts/Fire.cs
@@ -5,20 +5,38 @@ public class Fire : MonoBehaviour
 {
     [SerializeField] private EventReference eventFire;
     private EventInstance fireEventInstance;
+    bool isActive = false;
+    float distance = 0f;
     void Start()
     {
         fireEventInstance = RuntimeManager.CreateInstance(eventFire);
+        isActive = false;
     }
     public void StopFire()
     {
         fireEventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        isActive = false;
     }
     public void StartFire()
     {
         RuntimeManager.AttachInstanceToGameObject(fireEventInstance, transform);
+        fireEventInstance.setVolume(1.0f);
         fireEventInstance.start();
-        Debug.Log("Starting fire");
-        fireEventInstance.getPlaybackState(out var playbackState);
-        Debug.Log("Playback state: " + playbackState);
+        isActive = true;
+    }
+    public void Update()
+    {
+        if (isActive)
+        {
+            distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+            if(distance < 10f)
+            {
+                fireEventInstance.setParameterByName("Distance", distance);
+            }
+        }
+        else
+        {
+            fireEventInstance.setVolume(0.0f);
+        }
     }
 }
