0504792e2e65b66fb47e0bdee0a41477a90216a3
HeyJoeLang <heyjoelang@gmail.com>
2026-05-22 15:02:43 -0400

    Update Scenes

---
+++ Alien Influencer/Assets/Base/Scripts/Editor/SceneReferenceDrawer.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(SceneReference))]
+public class SceneReferenceDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+        EditorGUI.PropertyField(position, property.FindPropertyRelative("SceneAsset"), label);
+        EditorGUI.EndProperty();
+    }
+}
